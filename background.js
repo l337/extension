22953f01fe751a24d067993c5e93ca1841903a1c
1337 <autonomousmachine@gmail.com>
2018-08-02 02:13:27 -0700

    start getting the tabs with users folders, next use machine learning TensoorFlow?

--- background.js
+++ background.js
@@ -3,7 +3,7 @@
 const rootFolderName = [];
 
 function getFolderNames(bookmarkFolders) {
-	bookmarkFolders.map((bookmarkFolder) => {
+	bookmarkFolders.map(bookmarkFolder => {
 		if (bookmarkFolder.children) {
 			rootFolderName.push(bookmarkFolder.title);
 			getFolderNames(bookmarkFolder.children);
@@ -12,12 +12,17 @@ function getFolderNames(bookmarkFolders) {
 }
 
 
-chrome.browserAction.onClicked.addListener((tab) => { 
+chrome.browserAction.onClicked.addListener(tab => { 
 	chrome.bookmarks.getTree((bookmarks) => {
 		// bookmarks[0] - root folder
 		getFolderNames(bookmarks[0].children);
-
 		// TODO Get User All Tabs, then use machine learning algorithms to dictate
 		// what folders to place the the bookmarks in.
+		chrome.tabs.getAllInWindow(null, (tabs) => {
+			tabs.map(tab => {
+				// Do machine learning against the tabs and place them into the apporiate folder.
+				// chrome.bookmarks.create({'title': '*Bookmark Extension*'});
+			});
+		});
 	});
 });
